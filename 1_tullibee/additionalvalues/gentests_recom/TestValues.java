40ad27b605faf8d80eea8cc10a2b21590e03f560
denini08 <denini.gabriel@gmai.com>
2020-06-23 16:04:11 -0300

    Add factory methods and targets

--- 1_tullibee/additionalvalues/gentests_recom/TestValues.java
+++ 1_tullibee/additionalvalues/gentests_recom/TestValues.java
@@ -6,153 +6,205 @@ import com.ib.client.*;
 
 public class TestValues {
 
-    public static DataInputStream disOne() {
-        return new DataInputStream(new StringBufferInputStream("1\0"));
-    }
+	public static DataInputStream disOne() {
+		return new DataInputStream(new StringBufferInputStream("1\0"));
+	}
 
-    
-    // public static DataInputStream disOne() {
-    //     String action = "1";
-    //     String part1 = "2";
-    //     String part2 = "3";
-    //     String part3 = "4";
-    //     String part4 = "2.97";
-    //     String part5 = "1";
-    //     String end = "-1";
-    //     String all = action + "\0" + part1 + "\0" + part2 + "\0" + part3 + "\0" + part4 + "\0" + part5 + "\0" + end + "\0";
-    //     return new DataInputStream(new StringBufferInputStream(all));        
-    // }
+	// public static DataInputStream disOne() {
+	// String action = "1";
+	// String part1 = "2";
+	// String part2 = "3";
+	// String part3 = "4";
+	// String part4 = "2.97";
+	// String part5 = "1";
+	// String end = "-1";
+	// String all = action + "\0" + part1 + "\0" + part2 + "\0" + part3 + "\0" +
+	// part4 + "\0" + part5 + "\0" + end + "\0";
+	// return new DataInputStream(new StringBufferInputStream(all));
+	// }
 
+	// Denini Gabriel - TagValue.equals()
+	private static TagValue SingletonTagValue;
 
-    public static EWrapper eWrapperOne() {
+	public static TagValue denini4() {
+		if (SingletonTagValue == null) {
+			SingletonTagValue = new TagValue("Tag1", "Value1");
+		}
+		return SingletonTagValue;
+	}
 
-        return new com.ib.client.EWrapper() {
+	public static TagValue denini5() {
+		return denini4();
+	}
 
-	   	@Override
-	   	public void error(Exception e) { e.printStackTrace(); }
+	public static EWrapper eWrapperOne() {
 
-	   	@Override
-	   	public void error(String str) {}
+		return new com.ib.client.EWrapper() {
 
-	   	@Override
-	   	public void error(int id, int errorCode, String errorMsg) {	}
+			@Override
+			public void error(Exception e) {
+				e.printStackTrace();
+			}
 
-	   	@Override
-	   	public void connectionClosed() {}
+			@Override
+			public void error(String str) {
+			}
 
-	   	@Override
-	   	public void tickPrice(int tickerId, int field, double price, int canAutoExecute) {}
+			@Override
+			public void error(int id, int errorCode, String errorMsg) {
+			}
 
-	   	@Override
-	   	public void tickSize(int tickerId, int field, int size) {}
+			@Override
+			public void connectionClosed() {
+			}
 
-	   	@Override
-	   	public void tickOptionComputation(int tickerId, int field, double impliedVol, double delta,
-	   			double modelPrice, double pvDividend) {}
+			@Override
+			public void tickPrice(int tickerId, int field, double price, int canAutoExecute) {
+			}
 
-	   	@Override
-	   	public void tickGeneric(int tickerId, int tickType, double value) {	}
+			@Override
+			public void tickSize(int tickerId, int field, int size) {
+			}
 
-	   	@Override
-	   	public void tickString(int tickerId, int tickType, String value) {	}
+			@Override
+			public void tickOptionComputation(int tickerId, int field, double impliedVol, double delta,
+					double modelPrice, double pvDividend) {
+			}
 
-	   	@Override
-	   	public void tickEFP(int tickerId, int tickType, double basisPoints, String formattedBasisPoints,
-	   			double impliedFuture, int holdDays, String futureExpiry, double dividendImpact,
-	   			double dividendsToExpiry) {	}
+			@Override
+			public void tickGeneric(int tickerId, int tickType, double value) {
+			}
 
-	   	@Override
-	   	public void orderStatus(int orderId, String status, int filled, int remaining, double avgFillPrice,
-	   			int permId, int parentId, double lastFillPrice, int clientId, String whyHeld) {	}
+			@Override
+			public void tickString(int tickerId, int tickType, String value) {
+			}
 
-	   	@Override
-	   	public void openOrder(int orderId, Contract contract, Order order, OrderState orderState) {	}
+			@Override
+			public void tickEFP(int tickerId, int tickType, double basisPoints, String formattedBasisPoints,
+					double impliedFuture, int holdDays, String futureExpiry, double dividendImpact,
+					double dividendsToExpiry) {
+			}
 
-	   	@Override
-	   	public void openOrderEnd() {}
+			@Override
+			public void orderStatus(int orderId, String status, int filled, int remaining, double avgFillPrice,
+					int permId, int parentId, double lastFillPrice, int clientId, String whyHeld) {
+			}
 
-	   	@Override
-	   	public void updateAccountValue(String key, String value, String currency, String accountName) {	}
+			@Override
+			public void openOrder(int orderId, Contract contract, Order order, OrderState orderState) {
+			}
 
-	   	@Override
-	   	public void updatePortfolio(Contract contract, int position, double marketPrice, double marketValue,
-	   			double averageCost, double unrealizedPNL, double realizedPNL, String accountName) {	}
+			@Override
+			public void openOrderEnd() {
+			}
 
-	   	@Override
-	   	public void updateAccountTime(String timeStamp) {}
+			@Override
+			public void updateAccountValue(String key, String value, String currency, String accountName) {
+			}
 
-	   	@Override
-	   	public void accountDownloadEnd(String accountName) {}
+			@Override
+			public void updatePortfolio(Contract contract, int position, double marketPrice, double marketValue,
+					double averageCost, double unrealizedPNL, double realizedPNL, String accountName) {
+			}
 
-	   	@Override
-	   	public void nextValidId(int orderId) {}
+			@Override
+			public void updateAccountTime(String timeStamp) {
+			}
 
-	   	@Override
-	   	public void contractDetails(int reqId, ContractDetails contractDetails) {}
+			@Override
+			public void accountDownloadEnd(String accountName) {
+			}
 
-	   	@Override
-	   	public void bondContractDetails(int reqId, ContractDetails contractDetails) {}
+			@Override
+			public void nextValidId(int orderId) {
+			}
 
-	   	@Override
-	   	public void contractDetailsEnd(int reqId) {	}
+			@Override
+			public void contractDetails(int reqId, ContractDetails contractDetails) {
+			}
 
-	   	@Override
-	   	public void execDetails(int reqId, Contract contract, Execution execution) {}
+			@Override
+			public void bondContractDetails(int reqId, ContractDetails contractDetails) {
+			}
 
-	   	@Override
-	   	public void execDetailsEnd(int reqId) {	}
+			@Override
+			public void contractDetailsEnd(int reqId) {
+			}
 
-	   	@Override
-	   	public void updateMktDepth(int tickerId, int position, int operation, int side, double price, int size) {}
+			@Override
+			public void execDetails(int reqId, Contract contract, Execution execution) {
+			}
 
-	   	@Override
-	   	public void updateMktDepthL2(int tickerId, int position, String marketMaker, int operation, int side,
-	   			double price, int size) {}
+			@Override
+			public void execDetailsEnd(int reqId) {
+			}
 
-	   	@Override
-	   	public void updateNewsBulletin(int msgId, int msgType, String message, String origExchange) {	}
+			@Override
+			public void updateMktDepth(int tickerId, int position, int operation, int side, double price, int size) {
+			}
 
-	   	@Override
-	   	public void managedAccounts(String accountsList) {	}
+			@Override
+			public void updateMktDepthL2(int tickerId, int position, String marketMaker, int operation, int side,
+					double price, int size) {
+			}
 
-	   	@Override
-	   	public void receiveFA(int faDataType, String xml) {	}
+			@Override
+			public void updateNewsBulletin(int msgId, int msgType, String message, String origExchange) {
+			}
 
-	   	@Override
-	   	public void historicalData(int reqId, String date, double open, double high, double low, double close,
-	   			int volume, int count, double WAP, boolean hasGaps) {	}
+			@Override
+			public void managedAccounts(String accountsList) {
+			}
 
-	   	@Override
-	   	public void scannerParameters(String xml) {	}
+			@Override
+			public void receiveFA(int faDataType, String xml) {
+			}
 
-	   	@Override
-	   	public void scannerData(int reqId, int rank, ContractDetails contractDetails, String distance,
-	   			String benchmark, String projection, String legsStr) {	}
+			@Override
+			public void historicalData(int reqId, String date, double open, double high, double low, double close,
+					int volume, int count, double WAP, boolean hasGaps) {
+			}
 
-	   	@Override
-	   	public void scannerDataEnd(int reqId) {	}
+			@Override
+			public void scannerParameters(String xml) {
+			}
 
-	   	@Override
-	   	public void realtimeBar(int reqId, long time, double open, double high, double low, double close,
-	   			long volume, double wap, int count) {	}
+			@Override
+			public void scannerData(int reqId, int rank, ContractDetails contractDetails, String distance,
+					String benchmark, String projection, String legsStr) {
+			}
 
-	   	@Override
-	   	public void currentTime(long time) {	}
+			@Override
+			public void scannerDataEnd(int reqId) {
+			}
 
-	   	@Override
-	   	public void fundamentalData(int reqId, String data) {	}
+			@Override
+			public void realtimeBar(int reqId, long time, double open, double high, double low, double close,
+					long volume, double wap, int count) {
+			}
 
-	   	@Override
-	   	public void deltaNeutralValidation(int reqId, UnderComp underComp) {	}
+			@Override
+			public void currentTime(long time) {
+			}
 
-	   	@Override
-	   	public void tickSnapshotEnd(int reqId) {	}
+			@Override
+			public void fundamentalData(int reqId, String data) {
+			}
 
-	   	@Override
-	   	public void stopRequested() {		}
+			@Override
+			public void deltaNeutralValidation(int reqId, UnderComp underComp) {
+			}
 
-        };
-        
-    }    
-    
+			@Override
+			public void tickSnapshotEnd(int reqId) {
+			}
+
+			@Override
+			public void stopRequested() {
+			}
+
+		};
+
+	}
+
 }
