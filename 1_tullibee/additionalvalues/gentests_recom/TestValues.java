a5791f7856ad7c53c58fefda4b1dab47d623c877
cmlins <30838723+cmlins@users.noreply.github.com>
2020-06-25 23:10:30 -0300

    Update TestValues.java

--- 1_tullibee/additionalvalues/gentests_recom/TestValues.java
+++ 1_tullibee/additionalvalues/gentests_recom/TestValues.java
@@ -21,132 +21,204 @@ public class TestValues {
     //     String end = "-1";
     //     String all = action + "\0" + part1 + "\0" + part2 + "\0" + part3 + "\0" + part4 + "\0" + part5 + "\0" + end + "\0";
     //     return new DataInputStream(new StringBufferInputStream(all));        
-    // }
+	// }
+	
+	public static Contract contractif89() {
+		Contract a = new Contract();
+		a.equals(null);
 
+		return a;
+	}
 
-    public static EWrapper eWrapperOne() {
+	public static Contract contractif93() {
+		Contract a = new Contract();
+		a.equals(a);
 
-        return new com.ib.client.EWrapper() {
+		return a;
+	}
 
-	   	@Override
-	   	public void error(Exception e) { e.printStackTrace(); }
+	public static Execution executionEquals71() {
+		Execution a = new Execution();
+		a.equals(a);
 
-	   	@Override
-	   	public void error(String str) {}
+		return a;
+	}
 
-	   	@Override
-	   	public void error(int id, int errorCode, String errorMsg) {	}
+	public static Execution executionIf73() {
+		Execution a = new Execution();
+		a.equals(null);
 
-	   	@Override
-	   	public void connectionClosed() {}
+		return a;
+	}
 
-	   	@Override
-	   	public void tickPrice(int tickerId, int field, double price, int canAutoExecute) {}
+	public static EWrapper eWrapperOne() {
 
-	   	@Override
-	   	public void tickSize(int tickerId, int field, int size) {}
+		return new com.ib.client.EWrapper() {
 
-	   	@Override
-	   	public void tickOptionComputation(int tickerId, int field, double impliedVol, double delta,
-	   			double modelPrice, double pvDividend) {}
+			@Override
+			public void error(final Exception e) {
+				e.printStackTrace();
+			}
 
-	   	@Override
-	   	public void tickGeneric(int tickerId, int tickType, double value) {	}
+			@Override
+			public void error(final String str) {
+			}
 
-	   	@Override
-	   	public void tickString(int tickerId, int tickType, String value) {	}
+			@Override
+			public void error(final int id, final int errorCode, final String errorMsg) {
+			}
 
-	   	@Override
-	   	public void tickEFP(int tickerId, int tickType, double basisPoints, String formattedBasisPoints,
-	   			double impliedFuture, int holdDays, String futureExpiry, double dividendImpact,
-	   			double dividendsToExpiry) {	}
+			@Override
+			public void connectionClosed() {
+			}
 
-	   	@Override
-	   	public void orderStatus(int orderId, String status, int filled, int remaining, double avgFillPrice,
-	   			int permId, int parentId, double lastFillPrice, int clientId, String whyHeld) {	}
+			@Override
+			public void tickPrice(final int tickerId, final int field, final double price, final int canAutoExecute) {
+			}
 
-	   	@Override
-	   	public void openOrder(int orderId, Contract contract, Order order, OrderState orderState) {	}
+			@Override
+			public void tickSize(final int tickerId, final int field, final int size) {
+			}
 
-	   	@Override
-	   	public void openOrderEnd() {}
+			@Override
+			public void tickOptionComputation(final int tickerId, final int field, final double impliedVol,
+					final double delta, final double modelPrice, final double pvDividend) {
+			}
 
-	   	@Override
-	   	public void updateAccountValue(String key, String value, String currency, String accountName) {	}
+			@Override
+			public void tickGeneric(final int tickerId, final int tickType, final double value) {
+			}
 
-	   	@Override
-	   	public void updatePortfolio(Contract contract, int position, double marketPrice, double marketValue,
-	   			double averageCost, double unrealizedPNL, double realizedPNL, String accountName) {	}
+			@Override
+			public void tickString(final int tickerId, final int tickType, final String value) {
+			}
 
-	   	@Override
-	   	public void updateAccountTime(String timeStamp) {}
+			@Override
+			public void tickEFP(final int tickerId, final int tickType, final double basisPoints,
+					final String formattedBasisPoints, final double impliedFuture, final int holdDays,
+					final String futureExpiry, final double dividendImpact, final double dividendsToExpiry) {
+			}
 
-	   	@Override
-	   	public void accountDownloadEnd(String accountName) {}
+			@Override
+			public void orderStatus(final int orderId, final String status, final int filled, final int remaining,
+					final double avgFillPrice, final int permId, final int parentId, final double lastFillPrice,
+					final int clientId, final String whyHeld) {
+			}
 
-	   	@Override
-	   	public void nextValidId(int orderId) {}
+			@Override
+			public void openOrder(final int orderId, final Contract contract, final Order order,
+					final OrderState orderState) {
+			}
 
-	   	@Override
-	   	public void contractDetails(int reqId, ContractDetails contractDetails) {}
+			@Override
+			public void openOrderEnd() {
+			}
 
-	   	@Override
-	   	public void bondContractDetails(int reqId, ContractDetails contractDetails) {}
+			@Override
+			public void updateAccountValue(final String key, final String value, final String currency,
+					final String accountName) {
+			}
 
-	   	@Override
-	   	public void contractDetailsEnd(int reqId) {	}
+			@Override
+			public void updatePortfolio(final Contract contract, final int position, final double marketPrice,
+					final double marketValue, final double averageCost, final double unrealizedPNL,
+					final double realizedPNL, final String accountName) {
+			}
 
-	   	@Override
-	   	public void execDetails(int reqId, Contract contract, Execution execution) {}
+			@Override
+			public void updateAccountTime(final String timeStamp) {
+			}
 
-	   	@Override
-	   	public void execDetailsEnd(int reqId) {	}
+			@Override
+			public void accountDownloadEnd(final String accountName) {
+			}
 
-	   	@Override
-	   	public void updateMktDepth(int tickerId, int position, int operation, int side, double price, int size) {}
+			@Override
+			public void nextValidId(final int orderId) {
+			}
 
-	   	@Override
-	   	public void updateMktDepthL2(int tickerId, int position, String marketMaker, int operation, int side,
-	   			double price, int size) {}
+			@Override
+			public void contractDetails(final int reqId, final ContractDetails contractDetails) {
+			}
 
-	   	@Override
-	   	public void updateNewsBulletin(int msgId, int msgType, String message, String origExchange) {	}
+			@Override
+			public void bondContractDetails(final int reqId, final ContractDetails contractDetails) {
+			}
 
-	   	@Override
-	   	public void managedAccounts(String accountsList) {	}
+			@Override
+			public void contractDetailsEnd(final int reqId) {
+			}
 
-	   	@Override
-	   	public void receiveFA(int faDataType, String xml) {	}
+			@Override
+			public void execDetails(final int reqId, final Contract contract, final Execution execution) {
+			}
 
-	   	@Override
-	   	public void historicalData(int reqId, String date, double open, double high, double low, double close,
-	   			int volume, int count, double WAP, boolean hasGaps) {	}
+			@Override
+			public void execDetailsEnd(final int reqId) {
+			}
 
-	   	@Override
-	   	public void scannerParameters(String xml) {	}
+			@Override
+			public void updateMktDepth(final int tickerId, final int position, final int operation, final int side,
+					final double price, final int size) {
+			}
 
-	   	@Override
-	   	public void scannerData(int reqId, int rank, ContractDetails contractDetails, String distance,
-	   			String benchmark, String projection, String legsStr) {	}
+			@Override
+			public void updateMktDepthL2(final int tickerId, final int position, final String marketMaker,
+					final int operation, final int side, final double price, final int size) {
+			}
 
-	   	@Override
-	   	public void scannerDataEnd(int reqId) {	}
+			@Override
+			public void updateNewsBulletin(final int msgId, final int msgType, final String message,
+					final String origExchange) {
+			}
 
-	   	@Override
-	   	public void realtimeBar(int reqId, long time, double open, double high, double low, double close,
-	   			long volume, double wap, int count) {	}
+			@Override
+			public void managedAccounts(final String accountsList) {
+			}
 
-	   	@Override
-	   	public void currentTime(long time) {	}
+			@Override
+			public void receiveFA(final int faDataType, final String xml) {
+			}
 
-	   	@Override
-	   	public void fundamentalData(int reqId, String data) {	}
+			@Override
+			public void historicalData(final int reqId, final String date, final double open, final double high,
+					final double low, final double close, final int volume, final int count, final double WAP,
+					final boolean hasGaps) {
+			}
 
-	   	@Override
-	   	public void deltaNeutralValidation(int reqId, UnderComp underComp) {	}
+			@Override
+			public void scannerParameters(final String xml) {
+			}
 
-	   	@Override
-	   	public void tickSnapshotEnd(int reqId) {	}
+			@Override
+			public void scannerData(final int reqId, final int rank, final ContractDetails contractDetails,
+					final String distance, final String benchmark, final String projection, final String legsStr) {
+			}
+
+			@Override
+			public void scannerDataEnd(final int reqId) {
+			}
+
+			@Override
+			public void realtimeBar(final int reqId, final long time, final double open, final double high,
+					final double low, final double close, final long volume, final double wap, final int count) {
+			}
+
+			@Override
+			public void currentTime(final long time) {
+			}
+
+			@Override
+			public void fundamentalData(final int reqId, final String data) {
+			}
+
+			@Override
+			public void deltaNeutralValidation(final int reqId, final UnderComp underComp) {
+			}
+
+			@Override
+			public void tickSnapshotEnd(final int reqId) {
+			}
 
 	   	@Override
 	   	public void stopRequested() {		}
