4c9602104003005ba17c4b311634a33c24662184
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2020-06-27 10:55:18 -0300

    merging

--- 1_tullibee/additionalvalues/gentests_recom/TestValues.java
+++ 1_tullibee/additionalvalues/gentests_recom/TestValues.java
@@ -94,95 +94,90 @@ public class TestValues {
 	
     public static DataInputStream portfolioCaseDataStream() {
         String action = "7";
-	String version = "7";
-	String conId = "1";
+        String version = "7";
+        String conId = "1";
         String symbol = "a";
         String secType = "b";
         String expirity = "c";
-	String strike = "1.42";
-	String right = "d";
-	String multiplier = "e";
-	String primaryExch = "f";
-	String currency = "g";
-	String localSymbol = "h";
-	String position = "1";
-	String marketPrice = "1.5";
-	String marketValue = "2.1";
-	String averageCost = "1.1";
+        String strike = "1.42";
+        String right = "d";
+        String multiplier = "e";
+        String primaryExch = "f";
+        String currency = "g";
+        String localSymbol = "h";
+        String position = "1";
+        String marketPrice = "1.5";
+        String marketValue = "2.1";
+        String averageCost = "1.1";
         String unrealizedPNL = "2.2";
-	String realizedPNL = "3.1";
-	String accountName = "i";
-	String end = "-1";
-	String all = action + "\0" + version + "\0" + conId + "\0" + symbol + "\0" + secType + "\0" 
-		+ expirity + "\0" + strike + "\0" + right + "\0" + multiplier + "\0" + primaryExch + "\0"
-		+ currency + "\0" + localSymbol + "\0" + position + "\0" + marketPrice + "\0" + marketValue + "\0"
-		+ averageCost + "\0" + unrealizedPNL + "\0" + realizedPNL + "\0" + accountName + "\0" + end + "\0";
+        String realizedPNL = "3.1";
+        String accountName = "i";
+        String end = "-1";
+        String all = action + "\0" + version + "\0" + conId + "\0" + symbol + "\0" + secType + "\0" 
+            + expirity + "\0" + strike + "\0" + right + "\0" + multiplier + "\0" + primaryExch + "\0"
+            + currency + "\0" + localSymbol + "\0" + position + "\0" + marketPrice + "\0" + marketValue + "\0"
+            + averageCost + "\0" + unrealizedPNL + "\0" + realizedPNL + "\0" + accountName + "\0" + end + "\0";
         return new DataInputStream(new StringBufferInputStream(all));        
     }
-
+    
     public static DataInputStream portfolioCaseDataStream2() {
         String action = "7";
-	String version = "6";
-	String conId = "1";
+        String version = "6";
+        String conId = "1";
         String symbol = "a";
         String secType = "b";
         String expirity = "c";
-	String strike = "1.42";
-	String right = "d";
-	String currency = "g";
-	String localSymbol = "h";
-	String position = "1";
-	String marketPrice = "1.5";
-	String marketValue = "2.1";
-	String averageCost = "1.1";
+        String strike = "1.42";
+        String right = "d";
+        String currency = "g";
+        String localSymbol = "h";
+        String position = "1";
+        String marketPrice = "1.5";
+        String marketValue = "2.1";
+        String averageCost = "1.1";
         String unrealizedPNL = "2.2";
-	String realizedPNL = "3.1";
-	String accountName = "i";
-	String primaryExch = "ds";
-	String end = "-1";
-	String all = action + "\0" + version + "\0" + conId + "\0" + symbol + "\0" + secType + "\0" 
-		+ expirity + "\0" + strike + "\0" + right + "\0" + currency + "\0" + localSymbol + "\0" 
-		+ position + "\0" + marketPrice + "\0" + marketValue + "\0"+ averageCost + "\0" 
-		+ unrealizedPNL + "\0" + realizedPNL + "\0" + accountName + "\0" + primaryExch + "\0" + end + "\0";
+        String realizedPNL = "3.1";
+        String accountName = "i";
+        String primaryExch = "ds";
+        String end = "-1";
+        String all = action + "\0" + version + "\0" + conId + "\0" + symbol + "\0" + secType + "\0" 
+            + expirity + "\0" + strike + "\0" + right + "\0" + currency + "\0" + localSymbol + "\0" 
+            + position + "\0" + marketPrice + "\0" + marketValue + "\0"+ averageCost + "\0" 
+            + unrealizedPNL + "\0" + realizedPNL + "\0" + accountName + "\0" + primaryExch + "\0" + end + "\0";
         return new DataInputStream(new StringBufferInputStream(all));        
     }
-
+    
     public static DataInputStream acctUpdateTimeDatastream() {
         String action = "8";
-	String version = "6";
-	String timeStamp = "qw";
-	String end = "-1";
-	String all = action + "\0" + version + "\0" + timeStamp + end + "\0";
+        String version = "6";
+        String timeStamp = "qw";
+        String end = "-1";
+        String all = action + "\0" + version + "\0" + timeStamp + end + "\0";
         return new DataInputStream(new StringBufferInputStream(all));        
     }
 
-    public static int version() {
-	return 39;
-    }
-
-	// public static EReader reader() {
+    // public static EReader reader() {
 	// 	return new EReader(disOne(),eWrapperOne(), 39);
 	// }
-
+    
     public static EReader reader2() {
-	return new EReader(portfolioCaseDataStream(),eWrapperOne(), version());
+        return new EReader(portfolioCaseDataStream(),eWrapperOne(), version()); /* version() defined by Marcela */
     }
-
+    
     public static EReader reader3() {
-	return new EReader(portfolioCaseDataStream2(),eWrapperOne(), version());
-
+        return new EReader(portfolioCaseDataStream2(),eWrapperOne(), version());
     }
-
+    
     public static EReader reader4() {
-	return new EReader(acctUpdateTimeDatastream(), eWrapperOne(), version());
+        return new EReader(acctUpdateTimeDatastream(), eWrapperOne(), version());
     }
+    
   
+    /** Marcela **/
   
-  /** Marcela **/
-  
-	public static DataInputStream msgId() {
-		return new DataInputStream(new StringBufferInputStream("1\0"));
-	}
+    public static DataInputStream msgId() {
+        return new DataInputStream(new StringBufferInputStream("1\0"));
+    }
 
 
 	public static DataInputStream nextValidIdCase() {
